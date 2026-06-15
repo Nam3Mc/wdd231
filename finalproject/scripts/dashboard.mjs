94737ec109baa7a5de9e92dc089487185eb315bd
nam3mc <moralesdreiser5gmail.com>
2026-06-15 18:58:15 -0500

    last testing all good

--- finalproject/scripts/dashboard.mjs
+++ finalproject/scripts/dashboard.mjs
@@ -1,23 +1,28 @@
-import addClient from "./dashboard/add-client.mjs";
-import addService from "./dashboard/add-service.mjs";
-import addStock from "./dashboard/add-stock.mjs";
-import addTechnician from "./dashboard/add-technician.mjs";
-import clientsOptions from "./dashboard/clients-options.mjs";
-import comingServices from "./dashboard/comming-services.mjs";
 import roleVerificator from "./dashboard/role-verificator.mjs";
-import techniciansOptions from "./dashboard/technicians-options.mjs";
-import toggleForms from "./dashboard/toggle-forms.mjs";
 import hamburguerMenu from "./hamburguer-menu.mjs";
 import logOut from "./log-out.mjs";
+import toggleForms from "./dashboard/toggle-forms.mjs";
 
-roleVerificator()
-hamburguerMenu()
-logOut()
-toggleForms()
-addClient()
-addService()
-addTechnician()
-addStock()
-clientsOptions()
-techniciansOptions()
-comingServices()
+import clientsOptions from "./dashboard/clients-options.mjs";
+import techniciansOptions from "./dashboard/technicians-options.mjs";
+import comingServices from "./dashboard/comming-services.mjs";
+
+import addClient from "./dashboard/add-client.mjs";
+import addTechnician from "./dashboard/add-technician.mjs";
+import addService from "./dashboard/add-service.mjs";
+import addStock from "./dashboard/add-stock.mjs";
+
+roleVerificator();
+
+hamburguerMenu();
+logOut();
+toggleForms();
+
+clientsOptions();
+techniciansOptions();
+comingServices();
+
+addClient();
+addTechnician();
+addService();
+addStock();
